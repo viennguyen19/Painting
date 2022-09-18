77aee8b0fd2a1263e7b24b177bd0930fb38afe3d
Vien Nguyen <65631534+viennguyen19@users.noreply.github.com>
2022-09-18 02:44:09 +0000

    fix bugs for undo feature, and change layout

--- draw/draw.js
+++ draw/draw.js
@@ -3,87 +3,97 @@ var canvases = document.getElementsByTagName("canvas");
 for(var i=0; i<canvases.length; i++)
 {
   canvas = canvases[i];
-
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 }
-/* drawStack: location of every move will be pushed to the stack
-   step: info of every step will be push to the stack
-   movePos: index of begining of step in drawStack
-   stepInfo: save where each step start on drawStack and style of draw*/
-let drawStack = [];
-let step = [];
-let movePos = 0;
-let stepInfo = {start: 0, info: "stroke"};
-let holdPos = {posX: 0, posY: 0};
-let drawStyle = "stroke";
 
-let undoCount = 0;
+
+/* drawStack: location of every move will be pushed to the stack */
+let drawStack = [];
+let undoStack = [];
+let penColor = "black";
+let penSize = 1;
+let draw = false;
+let step = 0;
+
+let pen  = null;
+
 // context
 let savedCtx = document.getElementById("myCanvas").getContext("2d");
 savedCtx.fillStyle = "white";
 savedCtx.fillRect(0, 0, canvas.width, canvas.height);
 
-let lastX = -110;
-let lastY = 0;
-let penColor = "black";
-let penSize = 1;
+///////////////////////////////
+function saveStepInfo(stepXPos, stepYPos, stepColor, stepSize, stepCount, stepStack) {
+  stepStack.push({x:stepXPos, y:stepYPos, color:stepColor, size:stepSize, step: stepCount});
+}
 
-document.getElementById("myCanvas").style.cursor = "crosshair";
-document.getElementById("myCanvas").addEventListener("mousedown", function(event) {
-  let on = true;
-  lastX = event.clientX-110;
-  lastY = event.clientY;
-  // save location of mouse and push into the stack
-  var x = lastX;
-  var y = lastY;
-  drawStack.push({x,y});
-  
-  // save step info into the stack
-  stepInfo = {movePos, drawStyle};
-  step.push(stepInfo);
-  // move to next index in drawStack
-  movePos++;
-  this.addEventListener("mousemove", function(event) {
-    if(on) {
-      undoCount = 0;
-      var x = event.clientX-110;
-      var y = event.clientY;
-      // save location of mouse and push to stack
-      drawStack.push({x,y});
-      console.log(drawStack);
-      // move to next index in stack
-      movePos++;
-      dothis(x,y,this);
-    }
-  });
-  this.addEventListener("mouseup", function(event) {
-    on = false;
-    savedCtx.save();
-  });
-});
-
-function dothis(x,y,myelement) {
-  var ctx = myelement.getContext("2d");
-  ctx.strokeStyle = penColor;
-  ctx.lineWidth = penSize;
+function drawLine(stepStack, endPoint, ctx) {
+  if(draw == false) {
+    return;
+  }
+  let currentStep = endPoint-1;
+  let lastStep = currentStep-1;
+  if(stepStack[lastStep].step != stepStack[currentStep].step) {
+    return;
+  }
+  ctx.strokeStyle = stepStack[lastStep].color;
+  ctx.lineWidth = stepStack[lastStep].size;
   ctx.beginPath();
   ctx.lineJoin = 'round';
   ctx.lineCap = 'round';
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(x,y);
+  ctx.moveTo(stepStack[lastStep].x, stepStack[lastStep].y);
+  ctx.lineTo(stepStack[currentStep].x, stepStack[currentStep].y);
   ctx.stroke();
-  lastX = x;
-  lastY = y;
 }
+
+function drawAll() {
+  draw = true;  
+  let length = drawStack.length - 2;
+  let i = 2;
+  while(i<length) {
+    drawLine(drawStack, i, savedCtx);
+    i++;
+  } 
+  draw = false;
+}
+
+///////////////////////////////
+
+
+
+document.getElementById("myCanvas").addEventListener("mousedown", function(event) {
+  console.log("start");
+  draw = true;
+  step++;
+  saveStepInfo(event.clientX, event.clientY, penColor, penSize, step, drawStack);
+    this.addEventListener("mousemove", function(event) {
+      if(draw) {
+        console.log("move");
+        saveStepInfo(event.clientX, event.clientY, penColor, penSize, step, drawStack);
+        drawLine(drawStack, drawStack.length, savedCtx);
+      }
+    });
+  
+  this.addEventListener("mouseup", function(event) {
+    console.log("done");
+    draw = false;
+  });  
+  this.addEventListener("mouseleave", function(event) {
+    console.log("done");
+    draw = false;
+  });  
+});
+
+
 /*----------------------------------------------- */
 document.getElementById("myCanvas").addEventListener("touchstart", function(event) {
   let on = true;
-  lastX = event.touches[0].clientX-110;
+  lastX = event.touches[0].clientX;
   lastY = event.touches[0].clientY;
   this.addEventListener("touchmove", function(event) {
     if(on) {
-      var x = event.touches[0].clientX-110;
+      var x = event.touches[0].clientX;
       var y = event.touches[0].clientY;
       dothis(x,y,this);
       event.preventDefault();
@@ -100,177 +110,87 @@ document.getElementById("myCanvas").addEventListener("touchstart", function(even
 function clearCanvas() {
   const context = document.getElementById("myCanvas").getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
-  removeEraserSize();
-  removeBrushSize();
-  removeColor();
+  saveStepInfo(-1, -1, penColor, penSize, ++step, drawStack);
 }
 /*----------------------------------------------- */
 // Undo
 function undo() {
-  savedCtx.restore();
-  console.log(step);
-  undoCount++;
-  if(undoCount<=step.length) {
-    let prevLoc = step[step.length-undoCount];
-    savedCtx.strokeStyle = "white";
-    for(k=0; k <30; k++) {
-      for(j=prevLoc.movePos; j<drawStack.length-1; ) {
-        savedCtx.moveTo(drawStack[j].x, drawStack[j].y);
-        j++;
-  			savedCtx.lineTo(drawStack[j].x, drawStack[j].y);
-      }
-    }
-    savedCtx.stroke();
-  }
-  removeEraserSize();
-  removeBrushSize();
-  //removeColor();
-}
-/*----------------------------------------------- */
-// Eraser 
-function eraser() {
-  penColor = "white";
-  penSize = 1;
-  if(document.querySelector("#eraserSize") == null) {
-    let temp1 = document.querySelector("#tools");
-    let eraserSize = document.createElement('div');
-    eraserSize.id = "eraserSize";
-    temp1.appendChild(eraserSize);
-    let temp2 = document.querySelector("#eraserSize");
-    let sizeEraser = document.createElement('div');
-    sizeEraser.id = "sizeEraser";
-    temp2.appendChild(sizeEraser);
-    changeEraserSize(temp2);
-  }
-  removeBrushSize();
-  removeColor();
-}
-
-function changeEraserSize(div) {
-  div.addEventListener("click", function clickEvent(e) {
-    console.log("Test here");
-    // e = Mouse click event.
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left; 
-    var y = e.clientY - rect.top;  
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
-    document.getElementById("sizeEraser").style.top = y + "px";
-    penSize = y/10;
-  });
-}
-
-function removeEraserSize() {
-  let node = document.querySelector("#eraserSize");
-  if(node) {
-    if (node.parentNode) {
-      node.parentNode.removeChild(node);
+  let which = drawStack[drawStack.length-1].step;
+  while(drawStack[drawStack.length-1].step == which) {
+    drawStack.pop();
+    if(drawStack.length == 0) {
+      break;
     }
   }
+  undoHelper();
+  drawAll();
 }
-/*----------------------------------------------- */
-// Brush 
-function brush() {
-  penSize = 1;
+
+function undoHelper() {
+  const context = document.getElementById("myCanvas").getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+/*----------------------------------------------- 
+
+
+
+/****************************Handle change brush color****************************/
+function black() {
   penColor = "black";
-  if(document.querySelector("#brushSize") == null) {
-    let temp1 = document.querySelector("#tools");
-    let brushSize = document.createElement('div');
-    brushSize.id = "brushSize";
-    temp1.appendChild(brushSize);
-
-    let temp2 = document.querySelector("#brushSize");
-    let sizeBrush = document.createElement('div');
-    sizeBrush.id = "sizeBrush";
-    temp2.appendChild(sizeBrush);
-
-    changeBrushSize(temp2);
-  }
-  removeEraserSize();
-  removeColor();
+}
+function silver() {
+  penColor = "silver";
 }
 
-function changeBrushSize(div) {
-  div.addEventListener("click", function clickEvent(e) {
-    console.log("Test here");
-    // e = Mouse click event.
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left; 
-    var y = e.clientY - rect.top;  
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
-    document.getElementById("sizeBrush").style.top = y + "px";
-    penSize = y/10;
-  });
+function gray() {
+  penColor = "gray";
+}
+function orange() {
+  penColor = "orange";
 }
 
-function removeBrushSize() {
-  let node = document.querySelector("#brushSize");
-  if(node) {
-    if (node.parentNode) {
-      node.parentNode.removeChild(node);
-    }
-  }
+function maroon() {
+  penColor = "maroon";
 }
-/*----------------------------------------------- */
-let hideColor = true;
-function unhide() {
-  if(hideColor) {
-    document.getElementById("colorTable").style.zIndex  = 10;
-  } else {
-    document.getElementById("colorTable").style.zIndex  = -10;
-  }
-  hideColor = !hideColor;
-  removeBrushSize();
-  removeEraserSize()
+function red() {
+  penColor = "red";
 }
 
-
-
-function pickRed() {
-  penColor = "#ff0000";
+function purple() {
+  penColor = "purple";
 }
-function pickRedPurple() {
-  penColor = "#953553";
+function fushsia() {
+  penColor = "fuchsia";
 }
 
-function pickPurple() {
-  penColor = "#800080";
-}
-function pickBluePurple() {
-  penColor = "#3007B6";
-}
-
-function pickBlue() {
-  penColor = "#0000ff";
-}
-function pickCBlueGreen() {
-  penColor = "#0d98ba";
-}
-
-function pickGreen() {
+function green() {
   penColor = "green";
 }
-function pickYellowGreen() {
-  penColor = "#9acd32";
+function lime() {
+  penColor = "lime";
 }
 
-function pickYellow() {
+function olive() {
+  penColor = "olive";
+}
+function yellow() {
   penColor = "yellow";
 }
-function pickYellowOrange() {
-  penColor = "#ffae42";
+function navy() {
+  penColor = "navy";
 }
+function blue() {
+  penColor = "blue";
+}
+function teal() {
+  penColor = "teal";
+}
+function aqua() {
+  penColor = "aqua";
+}
+/****************************Handle change brush color****************************/
 
-function pickOrange() {
-  penColor = "ffa500";
-}
-function pickRedOrange() {
-  penColor = "#ff5349";
-}
-function pickBlack() {
-  penColor = "black";
-}
-
-/*---------------------------------------- */
+/****************************Handle download image****************************/
 download_img = function() {
   let image = canvas.toDataURL();  
   let downloadLink = document.createElement("a");  
@@ -280,3 +200,152 @@ download_img = function() {
   downloadLink.click();
   document.body.removeChild(downloadLink);
 };
+/****************************Handle download image****************************/
+
+
+/****************************Handle change brush size****************************/
+var oldX = 0, newX = 0; // for storing X (horizontal) positions
+var element = document.getElementById("circle"); // The element you want to drag
+let rect = element.getBoundingClientRect();
+let leftMax = rect.x;
+let rightMax = rect.x + 100;
+
+// We do the dragging here
+function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  newX = oldX - e.clientX; // to calculate how much we have moved
+  oldX = e.clientX; // store current value to use for next move
+  let temp = element.offsetLeft - newX;
+  if(temp > 100) {
+    temp = 100;
+  } else if(temp < 0) {
+    temp = 0;
+  } else {
+    //
+  }
+  element.style.left = temp + "px"; // update left position
+  console.log(penSize);
+  penSize = element.offsetLeft/2;
+  console.log(penSize);
+}
+
+// we do this so there is not multiple event handlers
+function closeDragElement() {
+  document.onmouseup = null;
+  document.onmousemove = null;
+}
+
+// when mouse down on element attach mouse move and mouse up for document
+// so that if mouse goes outside element still drags
+function dragMouseDown(e) {
+  e = e || window.event;
+  e.preventDefault();
+  oldX = e.clientX; // store current value to use for mouse move calculation
+  document.onmouseup = closeDragElement;
+  document.onmousemove = elementDrag;
+}
+
+element.onmousedown = dragMouseDown;
+
+function brush() {
+  penColor = "black";
+  pen.style.display = "none";
+  document.getElementById("myCanvas").style.cursor = 'url("./img/pen.png"),auto';
+  document.getElementById("brushContainer").style.display = "flex";
+}
+
+function hideBrush() {
+  document.getElementById("brushContainer").style.display = "none";
+}
+/****************************Handle change brush size****************************/
+
+
+/****************************Handle eraser cursor****************************/
+let myCanvas = document.getElementById("myCanvas");
+myCanvas.onmousemove = consoleL;
+let added = false;
+function consoleL(e) {
+  e = e || window.event;
+  e.preventDefault();
+  if(!added) {
+    pen = document.createElement("div");
+    pen.id = "pen";
+    document.body.appendChild(pen);
+    added = true;
+  }
+
+  pen.style.width = penSize + "px";
+  pen.style.height = penSize + "px";
+  pen.style.left = e.clientX - (penSize/2)  + "px";
+  pen.style.top = e.clientY - (penSize/2)  + "px";
+  //console.log(pen);
+  //console.log(pen.style.width);
+}
+/****************************Handle eraser cursor****************************/
+
+
+/****************************Handle change eraser size****************************/
+var oldX1 = 0, newX1 = 0; // for storing X (horizontal) positions
+var element1 = document.getElementById("circle1"); // The element you want to drag
+
+
+// We do the dragging here
+function elementDrag1(e) {
+  e = e || window.event;
+  e.preventDefault();
+  newX1 = oldX1 - e.clientX; // to calculate how much we have moved
+  oldX1 = e.clientX; // store current value to use for next move
+  let temp = element1.offsetLeft - newX1;
+  if(temp > 100) {
+    temp = 100;
+  } else if(temp < 0) {
+    temp = 0;
+  } else {
+    //
+  }
+  element1.style.left = temp + "px"; // update left position
+  penSize = element1.offsetLeft/2;
+}
+
+
+
+// when mouse down on element attach mouse move and mouse up for document
+// so that if mouse goes outside element still drags
+function dragMouseDown1(e) {
+  e = e || window.event;
+  e.preventDefault();
+  oldX1 = e.clientX; // store current value to use for mouse move calculation
+  document.onmouseup = closeDragElement;
+  document.onmousemove = elementDrag1;
+}
+
+element1.onmousedown = dragMouseDown1;
+
+function eraser() {
+  document.getElementById("eraserContainer").style.display = "flex";
+  penColor = "white";
+  pen.style.display = "block";
+  penSize = 30;
+  document.getElementById("myCanvas").style.cursor = 'none';
+}
+
+function hideEraser() {
+  document.getElementById("eraserContainer").style.display = "none";
+}
+/****************************Handle change eraser size****************************/
+
+
+
+document.getElementById("myCanvas").addEventListener("mousedown", (e) => {
+  const brushContainer =  document.getElementById("brushContainer");
+  const eraserContainer = document.getElementById("eraserContainer");
+  const tools = document.getElementById("tools");
+  let target = e.target;
+  if(target != brushContainer || target != eraserContainer) {
+    hideEraser();
+    hideBrush();
+  } 
+});
+
+
--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function openapp() {
-  console.log("re");
-  window.location.href = "https://MouseMove.damco1.repl.co/draw/draw.html"
+  window.location.href = "https://Painting-Testing.chanhvien.repl.co/draw/draw.html"
 }
 
 function openContact() {
-  console.log("anime");
   var element = document.getElementById("myinfo");
   var mycontact = document.getElementById("contact");
-  console.log(element.classList.contains("appear"));
   if(element.classList.contains("appear"))
   {
     element.classList.remove("appear");
     mycontact.textContent = "Contact";
-    mycontact.style.background = "lavender";
-    mycontact.style.color = "black";
   } else {
     element.classList.add("appear");
     mycontact.textContent = "Hide Contact";
-    mycontact.style.background = "black";
-    mycontact.style.color = "lavender";
   }  
 }
 
